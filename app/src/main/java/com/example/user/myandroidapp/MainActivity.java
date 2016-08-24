554e10297c37d5b0056094d3d755885a21315305
Kuan-Ting Chou <r03944003@ntu.edu.tw>
2016-08-24 14:25:58 +0800

    set first radioBtn checked and finish button clicked logic

--- app/src/main/java/com/example/user/myandroidapp/MainActivity.java
+++ app/src/main/java/com/example/user/myandroidapp/MainActivity.java
@@ -6,11 +6,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    static final String[] pokemonNames = {"小火龍","傑尼龜","妙蛙種子"};
     TextView infoText;
     EditText nameEditText;
     RadioGroup optionsGroup;
@@ -35,6 +37,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(viewId == R.id.confirmButton) {
             //confirmButton was clicked
             Log.d("buttonTest","confirm-button was clicked");
+            String name = nameEditText.getText().toString();
+
+            int selectedRadioButtonId = optionsGroup.getCheckedRadioButtonId();
+            View selectedRadioButtonView = optionsGroup.findViewById(selectedRadioButtonId);
+            int selectedIndex = optionsGroup.indexOfChild(selectedRadioButtonView);
+
+            RadioButton selectedRadioButton = (RadioButton)selectedRadioButtonView;
+            String radioBtnText = selectedRadioButton.getText().toString();
+
+            String welcomeMessage = String.format(
+                    "你好, 訓練家%s 歡迎來到神奇寶貝的世界 你的第一個夥伴是%s",
+                    name,
+                    radioBtnText
+            );
+
+            infoText.setText(welcomeMessage);
         }
     }
 }
