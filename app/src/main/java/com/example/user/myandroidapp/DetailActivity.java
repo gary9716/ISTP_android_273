b135e8ada69d3cdb7c8a8deb59878ef084c1a3b4
Kuan-Ting Chou <r03944003@ntu.edu.tw>
2016-08-26 16:39:30 +0800

    finish first row of detail layout

---
+++ app/src/main/java/com/example/user/myandroidapp/DetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.myandroidapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 2016/8/26.
+ */
+public class DetailActivity extends AppCompatActivity {
+
+
+
+
+}
