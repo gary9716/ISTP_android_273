b45a147f0afff7cce08cfd204aa7b5801ef04423
Kuan-Ting Chou <r03944003@ntu.edu.tw>
2016-08-30 16:26:27 +0800

    add new Activity for testing fragment usage and add new layout

---
+++ app/src/main/java/com/example/user/myandroidapp/FragmentTestActivity.java
@@ -0,0 +1,20 @@
+package com.example.user.myandroidapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FragmentTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_test);
+
+
+
+
+
+    }
+
+
+}
