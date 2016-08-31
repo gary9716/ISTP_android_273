656ed478f8afebb0effd522d45cd7f6ac16f5fad
Kuan-Ting Chou <r03944003@ntu.edu.tw>
2016-08-31 14:15:53 +0800

    finish activity_drawer layout and add Navigation Drawer Library

---
+++ app/src/main/java/com/example/user/myandroidapp/DrawerActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.myandroidapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrawerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer);
+    }
+}
