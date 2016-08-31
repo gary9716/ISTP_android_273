9e6ac908c895755575a820e891110320db2ba387
Kuan-Ting Chou <r03944003@ntu.edu.tw>
2016-08-31 16:34:34 +0800

    add parse initialization

--- app/src/main/java/com/example/user/myandroidapp/MyApplication.java
+++ app/src/main/java/com/example/user/myandroidapp/MyApplication.java
@@ -6,6 +6,7 @@ import android.view.Display;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.parse.Parse;
 
 /**
  * Created by user on 2016/8/25.
@@ -15,6 +16,14 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+                .enableLocalDataStore()
+                .applicationId("aBriKu0h4EZgnb8Sft9Uv4HyDZHOj01WZQp3jPs1")
+                .clientKey("YJy27NUjuLfJaicKAFReic3gpCFxdemFsPrsQj05")
+                .server("https://parseapi.back4app.com/")
+                .build()
+        );
 
         DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
                 .cacheInMemory(true)
