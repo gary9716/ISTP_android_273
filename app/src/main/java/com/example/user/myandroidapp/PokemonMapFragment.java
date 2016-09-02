e71193fe543857f66fa34394a55f3293b28983e1
Kuan-Ting Chou <r03944003@ntu.edu.tw>
2016-09-02 16:20:09 +0800

    finish onCreateView and onViewCreated in PokemonMapFragment

---
+++ app/src/main/java/com/example/user/myandroidapp/PokemonMapFragment.java
@@ -0,0 +1,72 @@
+package com.example.user.myandroidapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+/**
+ * Created by user on 2016/9/2.
+ */
+public class PokemonMapFragment extends Fragment {
+
+    SupportMapFragment mapFragment;
+    GoogleMap map;
+    GoogleApiClient apiClient;
+    LocationRequest locationRequest;
+
+    public static PokemonMapFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        PokemonMapFragment fragment = new PokemonMapFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    View fragmentView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        if(fragmentView == null) {
+            fragmentView = inflater.inflate(R.layout.fragment_map, null);
+            mapFragment = SupportMapFragment.newInstance();
+            mapFragment.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(GoogleMap googleMap) {
+
+                }
+            });
+
+            setHasOptionsMenu(true);
+            setMenuVisibility(true);
+        }
+
+        return fragmentView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getChildFragmentManager().beginTransaction()
+                .replace(R.id.childFragmentContainer, mapFragment)
+                .commit();
+    }
+}
